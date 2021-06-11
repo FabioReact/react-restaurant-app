7bfc5f1b89392929660f84730e8fb08e77b2a32c
FabioReact <fabio.react.dev@gmail.com>
2021-06-11 15:44:35 +0200

    Add POST to AddCourse

--- src/pages/AddCourse/AddCourse.jsx
+++ src/pages/AddCourse/AddCourse.jsx
@@ -1,10 +1,36 @@
-import { useRef } from "react"
+import { useState, useRef } from "react"
 
 const AddCourse = () => {
 	const courseNameInput = useRef(null)
+	const descriptionInput = useRef(null)
 	const popularInput = useRef(null)
+	const [price, setPrice] = useState('500')
+
 	const submitForm = () => {
-		console.log(popularInput.current)
+		const courseName = courseNameInput.current.value
+		const description = descriptionInput.current.value
+		fetch('http://localhost:4000/courses', {
+			method: 'POST',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				name: courseName,
+				description: description,
+				price: price,
+				image: "/aucune.jpg",
+				categories: [],
+				popular: popularInput.current.checked,
+			})
+		}).then(res => res.json())
+		.then(data => console.log(data))
+		.catch(err => console.error(err))
+	}
+
+	const onChangePrice = (event) => {
+		// Je récupère la valeur tapée par l'utilisateur grâce à event.target.value
+		setPrice(event.target.value)
 	}
 	return (
 		<div className='flex h-screen bg-gray-200 items-center justify-center'>
@@ -19,9 +45,9 @@ const AddCourse = () => {
 							xmlns='http://www.w3.org/2000/svg'
 						>
 							<path
-								stroke-linecap='round'
-								stroke-linejoin='round'
-								stroke-width='2'
+								strokeLinecap='round'
+								strokeLinejoin='round'
+								strokeWidth='2'
 								d='M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4'
 							></path>
 						</svg>
@@ -53,6 +79,7 @@ const AddCourse = () => {
 						Description
 					</label>
 					<input
+						ref={descriptionInput}
 						className='py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent'
 						type='text'
 						placeholder='Description'
@@ -69,6 +96,8 @@ const AddCourse = () => {
 							type='number'
 							placeholder='Prix'
 							min='0'
+							value={price}
+							onChange={onChangePrice}
 						/>
 					</div>
 				</div>
@@ -110,9 +139,9 @@ const AddCourse = () => {
 									xmlns='http://www.w3.org/2000/svg'
 								>
 									<path
-										stroke-linecap='round'
-										stroke-linejoin='round'
-										stroke-width='2'
+										strokeLinecap='round'
+										strokeLinejoin='round'
+										strokeWidth='2'
 										d='M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z'
 									></path>
 								</svg>
